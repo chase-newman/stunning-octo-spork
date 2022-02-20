f5b5433202c279006902b041a9ecc741b56ea244
Chase Newman <chasejn01@gmail.com>
2022-02-20 17:18:34 -0500

    update logic and styles

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const num = prompt("Choose a number: 0 or 1");
+// const num = prompt("Choose a number: 0 or 1");
 
-const keys = ["key0001", "key0002"];
+// const keys = ["key0001", "key0002"];
 
-const urlCIK = `https://test-project-d14cc-default-rtdb.firebaseio.com/${keys[num]}.json`;
+// const urlCIK = `https://test-project-d14cc-default-rtdb.firebaseio.com/${keys[num]}.json`;
 
-fetch(urlCIK)
-    .then(response => response.json())
-    .then(data => console.log(data))
+// fetch(urlCIK)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+
+
+let movieLine = ["Chase", "Catherine", "Kenzie"];
+
+movieLine.pop();
+
+movieLine.unshift("Brenda")
+
+
+let colors = ["red", "orange", "yellow", "green", "blue", 
+              "indigo", "violet"];
+              
+colors.slice(0,3);
+
